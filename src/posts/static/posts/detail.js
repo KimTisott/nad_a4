cd485c78529e528eb8425b05134c4642ae2027cc
Kim Tisott <kim.nicolay@hotmail.com>
2024-04-05 17:20:00 -0400

    Implemented dropzones for post photos

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -19,10 +19,6 @@ const bodyInput = document.getElementById('id_body')
 
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
-backBtn.addEventListener('click', ()=>{
-    history.back()
-})
-
 $.ajax({
     type: 'GET',
     url: url,
